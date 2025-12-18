67fd64af6fb6fcfad491ad1e80e0790a9861f390
Zheng Luo <luozheng2002@sjtu.edu.cn>
2025-12-18 13:28:40 -0600

    collected llama perplexity

--- judge_config_slurm4.py
+++ judge_config_slurm4.py
@@ -1,5 +1,5 @@
 from codebase_rs import *
 
 
-config = JudgeConfig(LocalModel.Llama3_3_70B, JudgeExperiment.Perplexity(lang="en"))
+config = JudgeConfig(LocalModel.Llama3_3_70B, JudgeExperiment.Perplexity(lang="zh_cn"))
 
